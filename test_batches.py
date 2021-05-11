7688b89bd76f007b025e943330ef3d4289113e0e
Megan <megan.duca@cybergrx.com>
2021-05-10 20:59:08 -0600

    Continue chapter 1 tests

--- test_batches.py
+++ test_batches.py
@@ -1,3 +1,4 @@
+import pytest
 from datetime import date
 from model import Batch, OrderLine
 
@@ -11,17 +12,36 @@ def test_allocating_to_a_batch_reduces_the_available_quantity():
     assert batch.available_quantity == 18
 
 
+# Helper function to create a batch and line for a given sku
+def make_batch_and_line(sku, batch_qty, line_qty):
+    return (
+        Batch("batch-001", sku, batch_qty, eta=date.today()),
+        OrderLine("order-123", sku, line_qty),
+    )
+
+
 def test_can_allocate_to_batch_if_available_is_greater_than_required():
-    pass
+    large_batch, small_line = make_batch_and_line("ELEGANT-LAMP", 20, 2)
+    assert large_batch.can_allocate(small_line)
 
 
 def test_can_allocate_to_batch_if_available_is_equal_to_required():
-    pass
+    batch, line = make_batch_and_line("ELEGANT-LAMP", 2, 2)
+    assert batch.can_allocate(line)
 
 
 def test_cannot_allocate_to_batch_if_available_is_less_than_required():
-    pass
+    small_batch, large_line = make_batch_and_line("ELEGANT-LAMP", 2, 20)
+    assert small_batch.can_allocate(large_line) is False
+
+
+def test_cannot_allocate_to_batch_if_skus_do_not_match():
+    batch = Batch("batch-001", "NEW-CHAIR", 100, eta=None)
+    line = OrderLine("order-123", "OLD-CHAIR", 10)
+    assert batch.can_allocate(line) is False
 
 
 def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line("ELEGANT-LAMP", 20, 2)
     pass
+
