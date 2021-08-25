8e6e6aa15c7065b62f90ed602441318a7c02a2c5
Megan <megan.duca@cybergrx.com>
2021-08-25 11:59:54 -0600

    Fix import errors

--- src/allocation/entrypoints/flask_app.py
+++ src/allocation/entrypoints/flask_app.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+from allocation import config
+from allocation.adapters import orm
+from allocation.adapters import repository
+from allocation.domain import model
+
+
+# orm.start_mappers()
+get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
 
-@app.route('/data', methods=['GET', 'POST'])
-def getData(): # execute function when this endpoint queried.
-  return "Some Data"
+@app.route("/", methods=["GET"])
+def home_endpoint():
+    return 'hi'
 
-app.run(host='<host>', port='<port>')
+@app.route("/allocate", methods=["POST"])
+def allocate_endpoint():
+    session = get_session()
+    batches = repository.SqlAlchemyRepository(session).list()
+    line = model.OrderLine(
+        request.json["orderid"], request.json["sku"], request.json["qty"],
+    )
+    batchref = model.allocate(line, batches)
+
+    return {"batchref": batchref}, 201
