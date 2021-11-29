f5b820dcafd84ba45a5fee0933f594365dd324f2
Megan <megan.duca@cybergrx.com>
2021-11-28 20:13:13 -0700

    Change docker setup

--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name="allocation", version="0.2", packages=["allocation", "allocation.adapters", "allocation.domain", "allocation.entrypoints", "allocation.service_layer"],
+    name="allocation",
+    version="0.1",
+    packages=["allocation"],
 )
